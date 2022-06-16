86b593b882b41ed5e3ef56322d5c4e4216890666
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-06-16 00:06:26 -0700

    ?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { handleEvent } from "flareact";
+import { handleEvent } from 'flareact';
 
 /**
  * The DEBUG flag will do two things that help during development:
@@ -7,12 +7,16 @@ import { handleEvent } from "flareact";
  * 2. we will return an error message on exception in your Response rather
  *    than the default 404.html page.
  */
-const DEBUG = false;
+const DEBUG = true;
 
-addEventListener("fetch", (event) => {
+addEventListener('fetch', (event) => {
   try {
     event.respondWith(
-      handleEvent(event, require.context("./pages/", true, /\.(js|jsx|ts|tsx)$/), DEBUG)
+      handleEvent(
+        event,
+        require.context('./pages/', true, /\.(js|jsx|ts|tsx)$/),
+        DEBUG
+      )
     );
   } catch (e) {
     if (DEBUG) {
@@ -22,6 +26,6 @@ addEventListener("fetch", (event) => {
         })
       );
     }
-    event.respondWith(new Response("Internal Error", { status: 500 }));
+    event.respondWith(new Response('Internal Error', { status: 500 }));
   }
 });
